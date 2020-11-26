3a6a63a246e7dc993fa46d97141a981c7cf55926
billsjchw <billsjc@126.com>
2020-11-26 07:06:12 +0000

    lab3: use lock_client in lock_tester and yfs_client

--- lock_tester.cc
+++ lock_tester.cc
@@ -17,8 +17,8 @@
 int nt = 6; //XXX: lab1's rpc handlers are blocking. Since rpcs uses a thread pool of 10 threads, we cannot test more than 10 blocking rpc.
 std::string dst;
 // Lab2: Use lock_client_cache when you test lock_cache
-// lock_client **lc = new lock_client * [nt];
-lock_client_cache **lc = new lock_client_cache * [nt];
+lock_client **lc = new lock_client * [nt];
+// lock_client_cache **lc = new lock_client_cache * [nt];
 lock_protocol::lockid_t a = 1;
 lock_protocol::lockid_t b = 2;
 lock_protocol::lockid_t c = 3;
@@ -176,8 +176,8 @@ main(int argc, char *argv[])
     VERIFY(pthread_mutex_init(&count_mutex, NULL) == 0);
     printf("lock client\n");
     // Lab2: Use lock_client_cache when you test lock_cache
-    // for (int i = 0; i < nt; i++) lc[i] = new lock_client(dst);
-    for (int i = 0; i < nt; i++) lc[i] = new lock_client_cache(dst);
+    for (int i = 0; i < nt; i++) lc[i] = new lock_client(dst);
+    // for (int i = 0; i < nt; i++) lc[i] = new lock_client_cache(dst);
 
     if(!test || test == 1){
       test1();
